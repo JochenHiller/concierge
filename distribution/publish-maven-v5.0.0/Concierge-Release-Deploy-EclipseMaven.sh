b947c5ee21bef26a646d7a18adca54aff8e552a2
Jochen Hiller <jochen.hiller@telekom.de>
2018-10-29 14:05:21 +0100

    Prepare to publish to Eclipse Maven
    Signed-off-by: Jochen Hiller <jochen.hiller@telekom.de>

--- distribution/publish-maven-v5.0.0/Concierge-Release-Deploy-EclipseMaven.sh
+++ distribution/publish-maven-v5.0.0/Concierge-Release-Deploy-EclipseMaven.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# See:
 # https://wiki.eclipse.org/Services/Nexus
-
+# https://docs.gradle.org/current/userguide/maven_plugin.html#uploading_to_maven_repositories
+# https://repo.eclipse.org/content/repositories/concierge-releases/
+# https://repo.eclipse.org/content/repositories/concierge-snapshots/
 
 
 echo "Concierge-Release-Deploy-EclipseMaven.sh"
 
-exit 0
+(
+set -x
+cd ../..
+
+# we simple call gradle publish
+# when version is NOT snapshot it will do a publish into release repo
+# use --info to get more details during development
+
+./gradlew --info publish
+)
